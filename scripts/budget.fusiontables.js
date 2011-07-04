fd977c3437ea1b3f7c93f2a9d92a35444631f304
derek <devnull@localhost>
2011-07-04 13:38:30 -0500

    started on fund/department table building functions

--- scripts/budget.fusiontables.js
+++ scripts/budget.fusiontables.js
@@ -1,15 +1,22 @@
 	var fusionTableId = 1086289;
   	google.load('visualization', '1', {}); //using Google visulaization API to do Fusion Tables SQL calls
   	var returnedData = "";
-  	var appropTotalArray = "";
-  	var expendTotalArray = "";
+  	var appropTotalArray;
+  	var expendTotalArray;
   	
   	function setTotalArrays() {
-  		getFundTotalArray('', true);
-  		getFundTotalArray('', false);
+  		getFundTotalArray('', true, updateAppropTotal);
+  		getFundTotalArray('', false, updateExpendTotal);
   	}
   	
-	function getFundTotalArray(fundName, isAppropriation) {
+  	function setFundTotals(year) {
+  		getFundTotalsForYear('Public Safety', year, getDataAsBudgetTable);
+  		getFundTotalsForYear('Corporate', year, getDataAsBudgetTable);
+  		getFundTotalsForYear('Election', year, getDataAsBudgetTable);
+  		getFundTotalsForYear('Health', year, getDataAsBudgetTable);
+  	}
+  	
+	function getFundTotalArray(fundName, isAppropriation, callback) {
 		var typeStr = "Expenditures";
 		if (isAppropriation == true) 
 			typeStr = "Appropriations";
@@ -26,29 +33,21 @@
 		if (fundName != '')
 			myQuery += " WHERE 'Fund' = '" + fundName + "'";
 		
-		if (isAppropriation == true)
-			getQuery(myQuery).send(updateAppropTotal);
-		else
-			getQuery(myQuery).send(updateExpendTotal);
+		getQuery(myQuery).send(callback);
 	}
 	
-	function getFundTotalForYear(fundName, year, isAppropriation) {
-		var typeStr = "Expenditures";
-		if (isAppropriation) 
-			typeStr = "Appropriations";
-		
-		var myQuery = "SELECT SUM('" + typeStr + " " + year + "') AS '" + year + "' FROM " + fusionTableId;
+	function getFundTotalsForYear(fundName, year, callback) {		
+		var myQuery = "SELECT Fund AS '"+ fundName +"', SUM('Appropriations " + year + "') AS 'Appropriations', SUM('Expenditures " + year + "') AS 'Expenditures' FROM " + fusionTableId;
 		if (fundName != '')
-			myQuery += " WHERE 'Fund' = '" + fundName + "'";
+			myQuery += " WHERE 'Fund' = '" + fundName + " Fund'";
 			
-		getQuery(myQuery).send(getDataAsCsv);
+		getQuery(myQuery).send(callback);
 	}
 	
 	function getQuery(myQuery) {
 		//alert(myQuery);
 		var queryText = encodeURIComponent(myQuery);
 	  	return query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryText);
-	  	//query.send(getDataAsTable);
 	}
 	
 	function updateAppropTotal(response) {
@@ -61,15 +60,8 @@
 		updateDisplay();
 	}
 	
-	//define callback function, this is called when the results are returned
 	function getDataAsArray(response) {
-	
-	  //for more information on the response object, see the documentation
-	  //http://code.google.com/apis/visualization/documentation/reference.html#QueryResponse
-	  numRows = response.getDataTable().getNumberOfRows();
 	  numCols = response.getDataTable().getNumberOfColumns();
-	  
-	  //concatenate the results into a string, you can build a table here
 	  var fusiontabledata = new Array();
 	  
 	  for(j = 0; j < numCols; j++) {
@@ -79,8 +71,23 @@
 	  return fusiontabledata;
 	}
 	
+	function getDataAsBudgetTable(response) {	
+	  fusiontabledata = "<tr>";
+	  fusiontabledata += "<td><a href='#'>" + response.getDataTable().getColumnLabel(0) + "</a></td>";
+	  fusiontabledata += "<td class='num budgeted'>" + response.getDataTable().getValue(0, 1) + "</td>";
+	  fusiontabledata += "<td class='num spent'>" + response.getDataTable().getValue(0, 2) + "</td>";
+	  fusiontabledata += "<td><div class='bars'>";
+      fusiontabledata += "          <span class='budgeted outer' style='width: 100%'></span>";
+      fusiontabledata += "          <span class='spent inner' style='width: 79.9%'></span>";
+      fusiontabledata += "        </div></td>";
+	  fusiontabledata += "</tr>";
+ 
+	  returnedData = fusiontabledata;
+	  updateTable();
+	}
+	
 	//for debugging - prints out data in a table
-	function getDataAsTable(response, year) {
+	function getDataAsTable(response) {
 	
 	  //for more information on the response object, see the documentation
 	  //http://code.google.com/apis/visualization/documentation/reference.html#QueryResponse
@@ -104,5 +111,5 @@
 	  fusiontabledata += "</table>";  
 	  //alert(fusiontabledata);
 	  returnedData += fusiontabledata;
-	  updateDisplay();
+	  updateTable();
 	}
